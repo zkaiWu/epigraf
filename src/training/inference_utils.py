91eac997294a542c97c05117e24148ca77832ace
zkaiWu <SY2221114@buaa.edu.cn>
2023-07-21 12:30:30 +0800

    commit test

--- src/training/inference_utils.py
+++ src/training/inference_utils.py
@@ -31,7 +31,6 @@ def setup_snapshot_image_grid(training_set, cfg, random_seed=0):
     else:
         camera_angles = sample_camera_angles(cfg=cfg.dataset.camera, batch_size=len(batch), device='cpu').numpy()
 
-
     return (gw, gh), np.stack(images), np.stack(labels), np.stack(camera_angles)
 
 #----------------------------------------------------------------------------
