5aeb8b70919ffb01c76dcb00fa7c607527b13dcd
zkaiWu <SY2221114@buaa.edu.cn>
2023-07-22 11:00:25 +0800

    modify dataset_epigraf.json to run

--- scripts/training/training_test.sh
+++ scripts/training/training_test.sh
@@ -4,17 +4,17 @@ CIDX=$1
 NUM_GPU=$2
 BATCH_SIZE=$3
 
-CUDA_VISIBLE_DEVICES={$CIDX} python src/infra/launch.py hydra.run.dir=. \
-    desc=eg3d_fake_saveimage_debugging \
+CUDA_VISIBLE_DEVICES=$CIDX python src/infra/launch.py hydra.run.dir=. \
+    desc=eg3d_fake_gpc0.5_noaug_fixpose2 \
     dataset=ffhq_posed \
     dataset.path=/home2/zhongkaiwu/data/dreamfusion_data/eg3d_fake/eg3d_generation_data_g4.0_noise500_long_prompt \
     dataset.resolution=256 \
     training.gamma=0.1 \
     training.resume=null \
-    num_gpus={$NUM_GPU} \
-    training.batch_size={$BATCH_SIZE} \
-    model.discriminator.camera_cond=false \
-    model.discriminator.camera_cond_raw=false \
-    model.generator.camera_cond=false \
+    num_gpus=$NUM_GPU \
+    training.batch_size=$BATCH_SIZE \
+    model.discriminator.camera_cond=true \
+    model.discriminator.camera_cond_raw=true \
+    model.generator.camera_cond=true \
     training.augment.mode=noaug \
     training.kimg=10000 \
