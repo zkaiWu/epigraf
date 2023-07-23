222f443025594ab242fb843497066731cbeb6439
zkaiWu <SY2221114@buaa.edu.cn>
2023-07-23 22:07:43 +0800

    submit for running

--- scripts/training/training_test.sh
+++ scripts/training/training_test.sh
@@ -5,9 +5,9 @@ NUM_GPU=$2
 BATCH_SIZE=$3
 
 CUDA_VISIBLE_DEVICES=$CIDX python src/infra/launch.py hydra.run.dir=. \
-    desc=eg3d_fake_gpc0.5_noaug_fixpose2 \
+    desc=eg3d_fake_woGPC_wDPC_noaug_fixpose2 \
     dataset=ffhq_posed \
-    dataset.path=/home2/zhongkaiwu/data/dreamfusion_data/eg3d_fake/eg3d_generation_data_g4.0_noise500_long_prompt \
+    dataset.path=/home/zhongkaiwu/data/dreamfusion_data/eg3d_fake/eg3d_generation_data_g4.0_noise500_long_prompt \
     dataset.resolution=256 \
     training.gamma=0.1 \
     training.resume=null \
@@ -15,6 +15,6 @@ CUDA_VISIBLE_DEVICES=$CIDX python src/infra/launch.py hydra.run.dir=. \
     training.batch_size=$BATCH_SIZE \
     model.discriminator.camera_cond=true \
     model.discriminator.camera_cond_raw=true \
-    model.generator.camera_cond=true \
+    model.generator.camera_cond=false \
     training.augment.mode=noaug \
     training.kimg=10000 \
