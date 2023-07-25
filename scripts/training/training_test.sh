c55abf995f6e148d70c4e71ccdc52af86e1f75df
zkaiWu <SY2221114@buaa.edu.cn>
2023-07-26 01:36:28 +0800

    modify scripts to train

--- scripts/training/training_test.sh
+++ scripts/training/training_test.sh
@@ -7,7 +7,7 @@ BATCH_SIZE=$3
 CUDA_VISIBLE_DEVICES=$CIDX python src/infra/launch.py hydra.run.dir=. \
     desc=epigraf_fake_noCond4D_Wcond4G_noaug \
     dataset=ffhq_posed \
-    dataset.path=/home/zhongkaiwu/data/dreamfusion_data/eg3d_fake/eg3d_generation_data_g4.0_noise500_long_prompt \
+    dataset.path=/home/zhongkaiwu/data/dreamfusion_data/epigraf_fake/epigraf_generation_data_g4.0_noise500_long_prompt \
     dataset.resolution=256 \
     training.gamma=0.1 \
     training.resume=null \
