6a2685ea0f1be034991b2909732add1040b1ab9d
zkaiWu <SY2221114@buaa.edu.cn>
2023-07-26 01:32:29 +0800

    modify scripts to train

--- scripts/training/training_test.sh
+++ scripts/training/training_test.sh
@@ -5,17 +5,17 @@ NUM_GPU=$2
 BATCH_SIZE=$3
 
 CUDA_VISIBLE_DEVICES=$CIDX python src/infra/launch.py hydra.run.dir=. \
-    desc=eg3d_fake_noCCondForDandG_noaug \
+    desc=epigraf_fake_noCond4D_Wcond4G_noaug \
     dataset=ffhq_posed \
-    dataset.path=/home2/zhongkaiwu/data/dreamfusion_data/eg3d_fake/eg3d_generation_data_g4.0_noise500_long_prompt \
+    dataset.path=/home/zhongkaiwu/data/dreamfusion_data/eg3d_fake/eg3d_generation_data_g4.0_noise500_long_prompt \
     dataset.resolution=256 \
-    training.gamma=1 \
+    training.gamma=0.1 \
     training.resume=null \
     num_gpus=$NUM_GPU \
     training.batch_size=$BATCH_SIZE \
     model.discriminator.camera_cond=false \
     model.discriminator.camera_cond_raw=false \
-    model.generator.camera_cond=false \
+    model.generator.camera_cond=true \
     model.loss_kwargs.density_loss=0 \
     training.augment.mode=noaug \
     training.kimg=10000 \
